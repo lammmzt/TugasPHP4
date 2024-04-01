eacd96b3cc5ba4dd22571f599db66bf26992a22d
lammmzt <alam0098@stmik-wp.ac.id>
2024-04-01 16:15:32 +0700

    add

---
+++ bawah.php
@@ -0,0 +1,13 @@
+<div style="text-align:center;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: white;
+    text-align: center;">
+    Copyright @M. NURUL ALAM - 2024
+</div>
+</body>
+
+</html>
